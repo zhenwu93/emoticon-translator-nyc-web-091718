74275cd8ef3d5c6c1a22d658f4f7914271bc3125
Zhen Wei Wu <zhenwu93@gmail.com>
2018-09-17 00:14:05 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,13 @@
-# require modules here
+require 'yaml'
 
 def load_library
-  # code goes here
+  library = YAML.load_file(file_path)
+  result = {"get_meaning" => {}, "get_emoticon" => {}}
+  library.each do |meaning, emoticons|
+    result["get_meaning"][emoticons[1]] = meaning
+    result["get_emoticon"][emoticons[0]] = emoticons[1]
+  end
+  result
 end
 
 def get_japanese_emoticon
