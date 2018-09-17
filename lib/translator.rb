3cfb8ce92b9a78fba396a882937d3094fee0c23d
Zhen Wei Wu <zhenwu93@gmail.com>
2018-09-17 00:23:44 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -19,7 +19,7 @@ def get_japanese_emoticon(file_path = './lib/emoticons.yml', english_emoticon)
   end
 end
 
-def get_english_meaning(file_path = './lib/emoticons.yml', Japanese_emoticon)
+def get_english_meaning(file_path = './lib/emoticons.yml', japanese_emoticon)
   library = load_library(file_path)
   if library ["get_meaning"].include?(japanese_emoticon)
     library["get_meaning"][japanese_emoticon]
