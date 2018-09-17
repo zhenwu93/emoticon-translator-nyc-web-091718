048c22e0d48a28a1d400d705bc6573702f3235df
Zhen Wei Wu <zhenwu93@gmail.com>
2018-09-17 00:18:50 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -19,6 +19,11 @@ def get_japanese_emoticon(file_path = './lib/emoticons.yml', english_emoticon)
   end
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(file_path = './lib/emoticons.yml', Japanese_emoticon)
+  library = load_library(file_path)
+  if library ["get_meaning"].include?(japanese_emoticon)
+    library["get_meaning"][japanese_emoticon]
+  else
+    "Sorry, that emoticon was not found"
+  end
 end
